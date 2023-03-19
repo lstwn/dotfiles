a85d9fa7492e54fcbe0b3580a68343f718e8d4a9
Leo <lstwn@mailbox.org>
2023-03-19 14:35:20 +0100

    fix popup showing chars from layer below

--- nvim/plugin/dressing.lua
+++ nvim/plugin/dressing.lua
@@ -4,25 +4,19 @@ dressing.setup({
     input = {
         -- Set to false to disable the vim.ui.input implementation
         enabled = true,
-
         -- Default prompt string
         default_prompt = "Input:",
-
         -- Can be 'left', 'right', or 'center'
         prompt_align = "left",
-
         -- When true, <Esc> will close the modal
         insert_only = false,
-
         -- When true, input will start in insert mode.
         start_in_insert = false,
-
         -- These are passed to nvim_open_win
         anchor = "NW",
         border = "rounded",
         -- 'editor' and 'win' will default to being centered
         relative = "cursor",
-
         -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
         prefer_width = 40,
         width = nil,
@@ -30,15 +24,13 @@ dressing.setup({
         -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
         max_width = { 140, 0.9 },
         min_width = { 20, 0.2 },
-
         buf_options = {},
         win_options = {
             -- Window transparency (0-100)
-            winblend = 10,
+            winblend = 0,
             -- Disable line wrapping
             wrap = false,
         },
-
         -- Set to `false` to disable
         mappings = {
             n = {
@@ -52,31 +44,25 @@ dressing.setup({
                 ["<Down>"] = "HistoryNext",
             },
         },
-
         override = function(conf)
             -- This is the config that will be passed to nvim_open_win.
             -- Change values here to customize the layout
             return conf
         end,
-
         -- see :help dressing_get_config
         get_config = nil,
     },
     select = {
         -- Set to false to disable the vim.ui.select implementation
         enabled = true,
-
         -- Priority list of preferred vim.select implementations
         backend = { "telescope", "fzf_lua", "fzf", "builtin", "nui" },
-
         -- Trim trailing `:` from prompt
         trim_prompt = true,
-
         -- Options for telescope selector
         -- These are passed into the telescope picker directly. Can be used like:
         -- telescope = require('telescope.themes').get_ivy({...})
         telescope = require("telescope.themes").get_cursor(),
-
         -- Options for fzf selector
         fzf = {
             window = {
@@ -84,7 +70,6 @@ dressing.setup({
                 height = 0.4,
             },
         },
-
         -- Options for fzf_lua selector
         fzf_lua = {
             winopts = {
@@ -92,7 +77,6 @@ dressing.setup({
                 height = 0.4,
             },
         },
-
         -- Options for nui Menu
         nui = {
             position = "50%",
@@ -106,14 +90,13 @@ dressing.setup({
                 filetype = "DressingSelect",
             },
             win_options = {
-                winblend = 10,
+                winblend = 0,
             },
             max_width = 80,
             max_height = 40,
             min_width = 40,
             min_height = 10,
         },
-
         -- Options for built-in selector
         builtin = {
             -- These are passed to nvim_open_win
@@ -121,13 +104,11 @@ dressing.setup({
             border = "rounded",
             -- 'editor' and 'win' will default to being centered
             relative = "editor",
-
             buf_options = {},
             win_options = {
                 -- Window transparency (0-100)
-                winblend = 10,
+                winblend = 0,
             },
-
             -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
             -- the min_ and max_ options can be a list of mixed types.
             -- max_width = {140, 0.8} means "the lesser of 140 columns or 80% of total"
@@ -137,24 +118,20 @@ dressing.setup({
             height = nil,
             max_height = 0.9,
             min_height = { 10, 0.2 },
-
             -- Set to `false` to disable
             mappings = {
                 ["<Esc>"] = "Close",
                 ["<C-c>"] = "Close",
                 ["<CR>"] = "Confirm",
             },
-
             override = function(conf)
                 -- This is the config that will be passed to nvim_open_win.
                 -- Change values here to customize the layout
                 return conf
             end,
         },
-
         -- Used to override format_item. See :help dressing-format
         format_item_override = {},
-
         -- see :help dressing_get_config
         get_config = nil,
     },
