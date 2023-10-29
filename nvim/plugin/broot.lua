e17b810f7e8e603be352093dc30fa30c23717e3f
Leo <lstwn@mailbox.org>
2023-10-29 16:22:02 +0000

    broot config fix to open pdfs from broot in nvim

--- nvim/plugin/broot.lua
+++ nvim/plugin/broot.lua
@@ -1,6 +1,7 @@
 vim.g.broot_replace_netrw = true
 vim.g.broot_open_command = "open"
 vim.g.broot_shell_command = "sh -c"
+vim.g.broot_command = "broot --sort-by-type-dirs-first"
 vim.g.broot_redirect_command = ">"
 vim.g.broot_vim_conf = {
     'default_flags = "gh"',
