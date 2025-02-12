3e5193f6cb510d86347bd82a66c0f8433f5027df
Leo <lstwn@mailbox.org>
2025-02-12 17:20:40 +0100

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -40,8 +40,13 @@ set -g fish_pager_color_description white             # the color of the complet
 #set -g fish_pager_color_secondary              # the background color of the every second completion
 
 # brew path
-if test -d /usr/local/bin
-    fish_add_path "/usr/local/bin"
+if not test (type --query brew)
+    set --global --export HOMEBREW_PREFIX "/opt/homebrew";
+    set --global --export HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+    set --global --export HOMEBREW_REPOSITORY "/opt/homebrew";
+    fish_add_path --global --move --path "/opt/homebrew/bin" "/opt/homebrew/sbin";
+    if test -n "$MANPATH[1]"; set --global --export MANPATH '' $MANPATH; end;
+    if not contains "/opt/homebrew/share/info" $INFOPATH; set --global --export INFOPATH "/opt/homebrew/share/info" $INFOPATH; end;
 end
 
 # rust path
