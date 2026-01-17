3a4c295033d60c02611821214cf99c28c3b4df18
Leo Stewen <lstwn@mailbox.org>
2026-01-17 14:32:48 +0100

    Fish prompt with priorities and greedy use of space

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,6 +2,7 @@ function fish_prompt
     # Save last status before doing anything that might affect it.
     set -l last_status $status
 
+    # Prepare the elements of the prompt.
     set user (set_color --background green black)" $USER "
 
     set host (set_color --background red black)" $(prompt_hostname) "
@@ -17,16 +18,31 @@ function fish_prompt
     # Prompt git branch only if we are in a git repository.
     set -l git
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1
-        set git (set_color --background yellow black)" $(git rev-parse --abbrev-ref HEAD) "
+        set branch (git rev-parse --abbrev-ref HEAD | string shorten --max 20)
+        set git (set_color --background yellow black)" $branch "
     end
 
-    if test $COLUMNS -gt 64
-        string join '' -- $user $host $cwd $git $stat (set_color normal) ' '
-    else if test $COLUMNS -gt 48
-        string join '' -- $cwd $git $stat (set_color normal) ' '
-    else if test $COLUMNS -gt 32
-        string join '' -- $cwd $stat (set_color normal) ' '
-    else
-        string join '' -- $stat (set_color normal) ' '
+    # Decide which elements to print according to
+    # their priority and the available columns.
+    set order $user $host $cwd $git $stat
+    set active false false false false false
+    set prios 5 3 4 1 2
+    set used 3 # We account for the vim mode indicator.
+    for prio in $prios
+        set element $order[$prio]
+        set length (string length --visible "$element")
+        if test (math $used + $length) -gt $COLUMNS
+            break
+        end
+        set active[$prio] true
+        set used (math $used + $length)
     end
+
+    # Print the surviving elements.
+    for i in (seq 1 (count $order))
+        if $active[$i]
+            echo -n "$order[$i]"
+        end
+    end
+    echo -e "$(set_color normal)\n"
 end
