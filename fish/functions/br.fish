37cc7364b991572a9a28a4d0d619bc8bbe5c27f7
Leo Stewen <lstwn@mailbox.org>
2026-01-17 13:36:44 +0100

    dotfiles command to quickly edit dotfiles in this repo

--- fish/functions/br.fish
+++
@@ -1,1 +0,0 @@
-/Users/lstwn/Library/Application Support/org.dystroy.broot/launcher/fish/br.fish
---
+++ fish/functions/dotfiles.fish
@@ -0,0 +1,4 @@
+function dotfiles --description "Edit your dotfiles"
+    cd ~/Projects/dotfiles
+    $EDITOR .
+end
